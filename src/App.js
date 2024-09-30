3d57feb144b8b58e8646532c60465a6775d9a7dd
g00408684atu <G00408684@atu.ie>
2024-09-30 13:54:19 +0000

    exercise2 complete

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Content from './Components/content'
+
+function App() {
+  return (
+    <div className="App">
+      <Content></Content>
+    </div>
+  );
+}
+
+export default App;
