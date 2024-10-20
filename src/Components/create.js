c1097d24de7278c2caf8a1dd7eab5bd7ce3f9654
g00408684atu <G00408684@atu.ie>
2024-10-20 22:25:47 +0000

    comments lab for form

--- src/Components/create.js
+++ src/Components/create.js
@@ -1,10 +1,57 @@
-// defined a component and named "create"
-const Create = ()=>{
-    return(
-        <div>
-            <h1>This is my Create component!</h1>
+// Importing the useState hook from React to manage component state
+import { useState } from "react";
+
+function Create() {
+  // State hooks to manage input fields like title year poster
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [poster, setPoster] = useState('');
+// Function for form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+  }
+//  render the form and input fields / div forms is for each different input field
+  return (
+    <div className="text-center">
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            // Update the title/year/poster state when the input value changes
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
         </div>
-    );
+
+        <div className="form-group">
+          <label>Add Movie Year: </label>
+          <input type="text"
+            className="form-control"
+            value={year}
+            onChange={(e) => { setYear(e.target.value) }}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Add Movie Poster: </label>
+          <input type="text"
+            className="form-control"
+            value={poster}
+            onChange={(e) => { setPoster(e.target.value) }}
+          />
+        </div>
+
+        
+
+        <div>
+        <input type="submit" value="Add Movie" />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Create;
